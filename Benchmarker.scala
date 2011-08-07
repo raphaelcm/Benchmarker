4cb97eabac2e34a615b8595acd85cba5088207b3
Raphael Crawford-Marks <raphael.crawfordmarks@gmail.com>
2011-08-07 17:28:22 -0400

    took out some placeholder code

--- Benchmarker.scala
+++ Benchmarker.scala
@@ -15,7 +15,9 @@ object Benchmarker {
 	 */
 	def benchmark[T, R](f: (T) => R, inputs: Array[T]): Long = {
 		val l1 = Platform.currentTime
+		
 		for(i <- 0 until inputs.length) f(inputs(i))
+		
 		val l2 = Platform.currentTime
 		Platform.collectGarbage
 		
@@ -44,28 +46,6 @@ object Benchmarker {
 		
 		return l2 - l1
 	}
-
-	/** 
-	 * TO DO: Measure the performance of one function f over a delimited input file. 
-	 * Method is genericized for input and output types of f. 
-	 *
-	 * Usage:
-	 *   Benchmarker.benchmark[T, R](f[Function1], TODO)
-	 *
-	 * @param f Function1 ((T) => R)
-	 * @param inputs TODO
-	 * @return elapsed time in milliseconds (Long)
-	 */
-	/* TODO - Data File Input
-	def benchmark[T, R](f: (T) => R, inputs: TODO): Long = {
-		val l1 = System.currentTimeMillis
-		val inputsIterator = inputs.iterator
-		
-		while(inputsIterator.hasNext) f(inputsIterator.next)
-		
-		return System.currentTimeMillis - l1
-	}
-	*/
 
 	/** 
 	 * Measure the performance of a function over generated inputs g(0) to g(n) 
